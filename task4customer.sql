fbb47b1d79a686fb82f2ae8149089fc08044b4cd
avantibisane30 <avantibisane@5gmail.com>
2025-04-10 15:08:17 +0530

    4th task is completed

---
+++ task4customer.sql
@@ -0,0 +1,12 @@
+create table customer(
+       cust_id varchar primary key,
+       customer_name varchar,
+       segment varchar,
+       age int,
+       country varchar,
+       city varchar,
+       state varchar,
+       postal_code int,
+       region varchar
+)
+select * from customer
---
+++ task4product.sql
@@ -0,0 +1,7 @@
+create table product(
+       product_id varchar primary key,
+	   category varchar,
+	   sub_category varchar,
+	   product_name varchar
+)
+select * from product
